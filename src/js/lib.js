9358feebd280994af034e74f8eadf550916275b7
Saw-jan <saw.jan.grg3e@gmail.com>
2021-12-14 08:39:13 +0545

    added search functions

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,3 +1,9 @@
+const fetch = require("cross-fetch");
+
+// globals
+const FILTER = "EgIQAQ%253D%253D"; // only video
+const SEARCH_URL = "https://www.youtube.com/results";
+const VIDEO_URL = "https://www.youtube.com/watch";
 let isLoop = false;
 let isInit = true;
 
@@ -44,7 +50,7 @@ window.addEventListener("load", () => {
 
     // search
     const searchInput = document.getElementById("search");
-    searchInput.addEventListener("keyup", (event) => {
+    searchInput.addEventListener("keyup", async (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
             const searchTerm = event.target.value;
@@ -52,6 +58,9 @@ window.addEventListener("load", () => {
             // clear value and focus out
             event.target.value = "";
             event.target.blur();
+
+            // search on YouTube
+            await YTsearch(searchTerm);
         }
     });
 });
@@ -61,4 +70,64 @@ function clickEffect(event) {
     setTimeout(() => {
         event.target.classList.remove("clicked");
     }, 200);
+}
+
+async function YTsearch(searchText) {
+    const searchParams = new URLSearchParams({
+        search_query: searchText,
+        sp: FILTER,
+    });
+
+    const response = await fetch(`${SEARCH_URL}?${searchParams}`);
+    const html = await response.text();
+    const video = getFirstVideo(html);
+    getVideoInfo(video.videoId);
+}
+
+function getFirstVideo(html) {
+    let firstVideo = null;
+
+    const regex = /var\sytInitialData\s=\s.*{.*}(?=;(\s)?<\/script>)/g;
+    const found = html.match(regex)[0];
+    const json = found.replace("var ytInitialData = ", "");
+    const respObj = JSON.parse(json);
+    const contents =
+        respObj.contents.twoColumnSearchResultsRenderer.primaryContents
+            .sectionListRenderer.contents[0].itemSectionRenderer.contents;
+    for (const video of contents) {
+        if (video.hasOwnProperty("videoRenderer")) {
+            firstVideo = video;
+            break;
+        }
+    }
+    return firstVideo.videoRenderer;
+}
+
+async function getVideoInfo(videoId) {
+    const response = await fetch(`${VIDEO_URL}?v=${videoId}`);
+    const html = await response.text();
+    console.log(html);
+    videoInfo = getVideoDetails(html);
+    const streams = videoInfo.streamingData.adaptiveFormats;
+    console.log(videoInfo.videoDetails.title);
+    let audio_link = null;
+    for (const format of streams) {
+        if (format.mimeType.includes("audio")) {
+            audio_link = format.url;
+            console.log(format.url);
+            break;
+        }
+    }
+    // await download(audio_link)
+}
+
+function getVideoDetails(html) {
+    const regex =
+        /var\sytInitialPlayerResponse\s=\s.*{.*}(?=;(\s)?<\/script>)/g;
+    const found = html.match(regex)[0];
+    console.log(found);
+    const json = found.replace("var ytInitialPlayerResponse = ", "");
+    const respObj = JSON.parse(json);
+    const { playabilityStatus, videoDetails, streamingData } = respObj;
+    return { playabilityStatus, videoDetails, streamingData };
 }
