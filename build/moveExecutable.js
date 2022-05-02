7c906b9137c183ae288534113ea5bfe8fecf9da4
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2022-05-02 16:04:50 +0545

    generate md5 and sha256 hashes for executables after build

--- build/moveExecutable.js
+++ build/moveExecutable.js
@@ -1,19 +1,41 @@
-const fs = require("fs-extra");
-const { basename } = require("path");
+const { readFileSync, writeFileSync } = require("fs-extra");
+const { moveSync } = require("fs-extra");
+const { basename, parse, dirname, join } = require("path");
+const { createHash } = require("crypto");
+
+const artifactFolder = "artifacts";
 
 exports.default = (context) => {
-    const exPath = "executables";
     context.artifactPaths.forEach((artifact) => {
         console.info(
             `[AfterBuild] Moving artifact "${basename(
                 artifact
-            )}" to "${exPath}/${basename(artifact)}"`
+            )}" to "${artifactFolder}/${basename(artifact)}"`
         );
 
         let destination = artifact.split("/");
-        destination.splice(destination.length - 1, 0, exPath);
+        destination.splice(destination.length - 1, 0, artifactFolder);
         destination = destination.join("/");
 
-        fs.moveSync(artifact, destination, { overwrite: true });
+        moveSync(artifact, destination, { overwrite: true });
+        createHashFiles(destination, ["md5", "sha256"]);
     });
 };
+
+function generateHash(file, alg, digest = "hex") {
+    const buffer = readFileSync(file);
+    return createHash(alg).update(buffer).digest(digest);
+}
+
+function createHashFiles(file, algs = ["md5"]) {
+    const hashFilename = parse(file).name;
+    algs.forEach((alg) => {
+        const filePath = join(dirname(file), `${hashFilename}.${alg}`);
+        const hash = generateHash(file, alg);
+        createFile(filePath, hash);
+    });
+}
+
+function createFile(filename, data) {
+    writeFileSync(filename, data, { encoding: "utf8" });
+}
