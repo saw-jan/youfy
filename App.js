193fe4117bd005bbbce68d5d2bdd331bac33ee7a
Saw-jan <saw.jan.grg3e@gmail.com>
2022-11-25 10:16:18 +0545

    allow devtools on dev mode

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
+const windowConfig = {
+    width: 400,
+    height: 140,
+    frame: false,
+    resizable: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        devTools: false,
+    },
+};
+
 // enable live reload in 'dev' mode
 if (process.env.MODE === "dev") {
     require("electron-reload")(__dirname, {
         electron: require(path.join(__dirname, "node_modules", "electron")),
     });
+    windowConfig.resizable = true;
+    windowConfig.webPreferences.devTools = true;
 }
 
 const createWindow = () => {
-    const window = new BrowserWindow({
-        width: 400,
-        height: 140,
-        frame: false,
-        resizable: false,
-        titleBarStyle: "hidden",
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            devTools: false,
-        },
-    });
+    const window = new BrowserWindow(windowConfig);
 
     window.loadFile(path.join(__dirname, "dist", "index.html"));
 
